da9e417267bc2a11572fbadecd377d76e3ab37a9
Loreta <badalyan.loreta@gmail.com>
2024-03-26 23:08:17 +0400

    Selecting a Friend

--- src/components/FormSplitBill.jsx
+++ src/components/FormSplitBill.jsx
@@ -1,7 +1,7 @@
-const FormSplitBill = () => {
+const FormSplitBill = ({selectedFriend}) => {
   return (
     <form className="form-split-bill">
-      <h2>Split a bill with X</h2>
+      <h2>Split a bill with {selectedFriend.name}</h2>
 
       <label>💰 Bill value</label>
       <input type="text" />
@@ -9,13 +9,13 @@ const FormSplitBill = () => {
       <label>🧍‍♀️ Your expense</label>
       <input type="text" />
 
-      <label>👫 X's expense</label>
+      <label>👫 {selectedFriend.name}'s expense</label>
       <input type="text" disabled />
 
       <label>🤑 Who is paying the bill</label>
       <select>
         <option value="user">You</option>
-        <option value="friend">X</option>
+        <option value="friend">{selectedFriend.name}</option>
       </select>
     </form>
   );
--- src/components/FriendsList.jsx
+++ src/components/FriendsList.jsx
@@ -1,11 +1,14 @@
 import Friend from './Friend';
 
-
-function FriendsList({ friends}) {
+function FriendsList({ friends, onSelection, selectedFriend}) {
   return (
     <ul>
       {friends.map((friend) => (
-        <Friend friend={friend} key={friend.id} />
+        <Friend 
+        friend={friend} 
+        onSelection={onSelection}
+        selectedFriend={selectedFriend}
+        key={friend.id} />
       ))}
     </ul>
   );
